a4796ef5ffdd6c862258eed6019463df7bec3538
devrodrigobnu <devrodrigobnu@gmail.com>
2023-09-19 16:17:10 -0300

    update
    HTML E CSS

---
+++ aula_while/desafios while/ex39.js
@@ -0,0 +1,22 @@
+// Faça um programa que leia dez conjuntos 
+// de dois valores, o primeiro representando o
+// número do aluno e o segundo representando a 
+// sua altura em centímetros. Encontre o aluno 
+// mais alto e o mais baixo. Mostre o número do aluno 
+// mais alto e o número do aluno mais baixo, junto com
+// suas alturas.
+
+const req = require('prompt-sync')
+const prompt = req()
+
+
+numeroAluno = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+alturaAlunos = [1.50, 1.56, 1.80,
+                1.94, 1.66, 1,86,
+                1.99, 1.55, 1.76,
+                2.00]
+
+
+for (let = 0; i < 10; i++){
+    
+}
